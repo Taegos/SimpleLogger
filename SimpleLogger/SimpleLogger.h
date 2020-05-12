c9a746f3b810d92e47bed6f82191b66a0bf73320
Tageos <benjamin.fischr@gmail.com>
2020-05-12 23:41:09 +0200

    Better api

--- SimpleLogger/SimpleLogger.h
+++ SimpleLogger/SimpleLogger.h
@@ -1,32 +1,51 @@
 #pragma once
 #include <iostream>
 
-#define nl '\n'
-
 class SimpleLogger
 {
 public:
     SimpleLogger(bool enabled = true) : enabled{ enabled } {}
 
     template<typename T>
-    void operator()(T);
+    void out(T);
 
     template<typename T, typename ...Args>
-    void operator()(T, Args...);
+    void out(T, Args...);
+
+    template<typename T>
+    void outnl(T);
+
+    template<typename T, typename ...Args>
+    void outnl(T, Args...);
 
 private:
     const bool enabled;
 };
 
+template<>
+void SimpleLogger::out(float f) {
+    if (!enabled) return;
+    std::cout << f;
+}
+
 template<typename T>
-void SimpleLogger::operator()(T t) {
+void SimpleLogger::out(T t) {
     if (!enabled) return;
     std::cout << t;
 }
 
 template<typename T, typename ...Args>
-void SimpleLogger::operator()(T t, Args... args) {
-    if (!enabled) return;
-    std::cout << t;
-    (*this)(args...);
+void SimpleLogger::out(T t, Args... args) {
+    out(t);
+    out(args...);
+}
+
+template<typename T>
+void SimpleLogger::outnl(T t) {
+    out(t, '\n');
+}
+
+template<typename T, typename ...Args>
+void SimpleLogger::outnl(T t, Args... args) {
+    out(t, args..., '\n');
 }
--- Test/main.cpp
+++ Test/main.cpp
@@ -3,5 +3,6 @@
 int main()
 {
 	SimpleLogger logger;
-	logger("a");
+	//logger.out(1.140, 1.33, "asd");
+	//std::cout << std::endl << 1.14 << std::endl;
 }
