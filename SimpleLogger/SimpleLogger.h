6f934685623fe39a4edba10583cfc3f89c315985
Tageos <benjamin.fischr@gmail.com>
2020-05-17 13:15:24 +0200

    Specialization for wstring and float

--- SimpleLogger/SimpleLogger.h
+++ SimpleLogger/SimpleLogger.h
@@ -7,45 +7,51 @@ public:
     SimpleLogger(bool enabled = true) : enabled{ enabled } {}
 
     template<typename T>
-    void out(T);
+    void out(T) const;
 
     template<typename T, typename ...Args>
-    void out(T, Args...);
+    void out(T, Args...) const;
 
     template<typename T>
-    void outnl(T);
+    void outnl(T) const;
 
     template<typename T, typename ...Args>
-    void outnl(T, Args...);
+    void outnl(T, Args...) const;
 
 private:
     const bool enabled;
 };
 
 template<>
-void SimpleLogger::out(float f) {
+inline void SimpleLogger::out(std::wstring str) const {
+    if (!enabled) return;
+    std::wcout << str;
+}
+
+template<>
+inline void SimpleLogger::out(float f) const {
     if (!enabled) return;
     std::cout << f;
 }
 
 template<typename T>
-void SimpleLogger::out(T t) {
+void SimpleLogger::out(T t) const {
     if (!enabled) return;
     std::cout << t;
 }
 
 template<typename T, typename ...Args>
-void SimpleLogger::out(T t, Args... args) {
+void SimpleLogger::out(T t, Args... args)const {
     out(t);
     out(args...);
 }
 
 template<typename T>
-void SimpleLogger::outnl(T t) {
+void SimpleLogger::outnl(T t) const {
     out(t, '\n');
 }
 
 template<typename T, typename ...Args>
-void SimpleLogger::outnl(T t, Args... args) {
+void SimpleLogger::outnl(T t, Args... args) const {
     out(t, args..., '\n');
 }
